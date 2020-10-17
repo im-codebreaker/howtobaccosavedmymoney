3dfda83c3f313dcf74d4720cb810e623299dc4d0
MehdiHan <meh.bouchard@outlook.fr>
2020-10-17 10:40:15 +0200

    Adding img folder with brands' svg, new property url in prices.json

--- js/app.js
+++ js/app.js
@@ -1,7 +1,35 @@
 const startDateInput = document.getElementById('startDateInput');
 const cigarettesByDayInput = document.getElementById('cigarettesByDayInput');
 const packetPriceInput = document.getElementById('packetPriceInput');
+const packetPriceContainer = document.getElementById('packetPrice');
 const button = document.getElementById('button');
+const brandsContainer = document.getElementById('brandsContainer');
+
+brandsContainer.addEventListener('click', function (event) {
+  console.log(event);
+});
+const xhr = new XMLHttpRequest();
+xhr.open('GET', 'data/prices.json');
+xhr.send();
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === 4) {
+    const cigarettes = JSON.parse(xhr.responseText);
+    cigarettes.brands.forEach((brand) => {
+      createSvg(brand);
+    });
+  }
+};
+
+/**
+ * This function taking a brand object as argument and create an img element with svg url and brand name as alt
+ * @param {object} brand a brand of cigarettes
+ */
+const createSvg = (brand) => {
+  const img = document.createElement('img');
+  img.src = brand.url;
+  img.alt = brand.name;
+  brandsContainer.appendChild(img);
+};
 
 const saveMoneyAndTime = (dDay, startDay, cigarettesByDay, packetPrice) => {
   const intervalInSeconds = Math.floor((dDay - startDay) / 1000);
